f3059e856875e94cc8a75f3ef7494adab5cfdce3
i2097i <i2097i@hotmail.com>
2025-12-06 00:03:11 -0500

    add env vars
    added listen
    added debug logging

--- bin/cli/config.rb
+++ bin/cli/config.rb
@@ -12,24 +12,18 @@ module CLI
 
   # cli entrypoint
   def self.config
-    Aro::P.say("#{__FILE__}:#{__method__} was called (todo)...")
+    Aro::D.say("#{__FILE__}:#{__method__} was called (todo)...")
   end
 
   class Config
     include Singleton
 
-    VAR_PREFIX = :ARO_CONFIG_
+    ARO_CONFIG_PREFIX = :ARO_CONFIG_
+    ARO_ENV_PREFIX = :ARO_ENV_
 
-    CONFIG_FILE = :".config".to_s
-    CONFIG_FILE_PATH = "#{Aro::Db.base_aro_dir(Aro::Db.get_name_from_namefile)}/#{CLI::Config::CONFIG_FILE}"
+    CONFIG_FILE = :".config"
 
-    INTS = {
-      O:       0,
-      I:       1,
-      VII:     7,
-      XXII:    22,
-      MMXCVII: Aro::Mancy::I2097I[1..4].to_i,
-    }
+    DATE_FORMAT = "%Y:%m:%d:%H:%M:%S"
 
     # possible formats
     # 
@@ -49,6 +43,11 @@ module CLI
       RUBY_FACOT: :ruby_facot,
     }
 
+    DEF_ACCESS = {
+      READ: :read,
+      WRITE: :write
+    }
+
     # types used in definition
     # 
     # example usage:
@@ -57,30 +56,58 @@ module CLI
       INT: {
         name: :int,
         description: I18n.t("cli.config.type_int_description"),
-        validator: Proc.new{|unvalid, deff, key|
-          CLI::Config.def_valid?(deff, key) &&
-          CLI::Config.int_valid?(unvalid)   &&
-          unvalid.to_i <= deff[:min]        &&
-          unvalid.to_i >= deff[:max]
+        validator: Proc.new{|unvalid, k, v|
+          int_valid = CLI::Config.def_valid?(k, v) &&
+            CLI::Config.int_valid?(unvalid)        &&
+            unvalid.to_i >= v[:min]                &&
+            unvalid.to_i <= v[:max]
+
+          Aro::D.say("validating #{k} (#{CLI::Config::DEF_TYPES[:INT][:name]})")
+          Aro::D.say("unvalid = #{unvalid}")
+          Aro::D.say("unvalid is#{int_valid ? " " : :" not ".to_s}valid.")
+          int_valid
+        }
+      },
+      STRING: {
+        name: :string,
+        description: I18n.t("cli.config.type_string_description"),
+        validator: Proc.new{|unvalid, k, v|
+          unvalid.is_a?(String)
         }
       },
       VALUES: {
         name: :values,
         description: I18n.t("cli.config.type_values_description"),
-        validator: Proc.new{|unvalid, deff, key|
-          CLI::Config.def_valid?(deff, key) &&
-          deff[:possible_values].keys.include?(unvalid&.to_sym)
+        validator: Proc.new{|unvalid, k, v|
+          CLI::Config.def_valid?(k, v) &&
+          v[:possible_values].keys.include?(unvalid&.to_sym)
         }
       },
     }
+
+    def validate_config
+      invalid_defs = []
+      CLI::Config::DEF.each{|k, v|
+        var_value = CLI::Config.cvar(k)
+        is_valid = CLI::Config::DEF_TYPES[
+          v[:type].to_s.upcase.to_sym
+        ][:validator].call(var_value, k, v)
+        
+        invalid_defs << k unless is_valid
+      }
+      invalid_defs
+    end
 
     # adapts I18n translations to generate bash environment vars.
     # 
     # example usage:
     # CLI::Config::DEF[:Z_MAX]
     DEF = {
+
+      # writable variables
       FORMAT: { # not implemented yet.
         type: :values,
+        access: :write,
         description: I18n.t(
           "cli.config.format_description",
           possible_values: CLI::Config::FORMATS.values.join(", ")
@@ -93,6 +120,7 @@ module CLI
       },
       DIMENSION: {
         type: :values,
+        access: :write,
         description: I18n.t(
           "cli.config.dimension_description",
           possible_values: CLI::Config::DMS.values.join(", ")
@@ -103,143 +131,260 @@ module CLI
           ruby_facot: I18n.t("cli.config.dimension_ruby_facot_description"),
         }
       },
-      DRAW_COUNT: { # not implemented yet. replace Aro::Deck::DRAW_COUNT with this
+      DISPLAY_COLUMNS: {
         type: :int,
+        access: :write,
         description: I18n.t(
-          "cli.config.draw_count_description",
-          min: CLI::Config::INTS[:I],
-          max: CLI::Config::INTS[:XXII],
+          "cli.config.display_columns_description",
+          min: Aro::Mancy::NUMERALS[:I],
+          max: Aro::Mancy::NUMERALS[:XXII] / Aro::Mancy::OS,
         ),
-        value: CLI::Config::INTS[:VII],
-        min: CLI::Config::INTS[:I],
-        max: CLI::Config::INTS[:XXII],
+        value: Aro::Mancy::NUMERALS[:VII],
+        min: Aro::Mancy::NUMERALS[:I],
+        max: Aro::Mancy::NUMERALS[:XXII] / Aro::Mancy::OS,
       },
       Z: {
         type: :int,
+        access: :write,
         description: I18n.t(
           "cli.config.z_description",
-          min: CLI::Config::INTS[:I],
-          max: CLI::Config::INTS[:MMXCVII],
+          min: Aro::Mancy::NUMERALS[:I],
+          max: Aro::Mancy::NUMERALS[:MMXCVII],
         ),
-        value: CLI::Config::INTS[:I],
-        min: CLI::Config::INTS[:I],
-        max: CLI::Config::INTS[:MMXCVII],
+        value: Aro::Mancy::NUMERALS[:I],
+        min: Aro::Mancy::NUMERALS[:I],
+        max: Aro::Mancy::NUMERALS[:MMXCVII],
       },
       Z_MAX: {
         type: :int,
+        access: :write,
         description: I18n.t(
           "cli.config.z_max_description",
-          min: CLI::Config::INTS[:I],
-          max: CLI::Config::INTS[:XXII],
+          min: Aro::Mancy::NUMERALS[:I],
+          max: Aro::Mancy::NUMERALS[:XXII],
         ),
-        value: CLI::Config::INTS[:VII],
-        min: CLI::Config::INTS[:I],
-        max: CLI::Config::INTS[:XXII],
+        value: Aro::Mancy::NUMERALS[:VII],
+        min: Aro::Mancy::NUMERALS[:I],
+        max: Aro::Mancy::NUMERALS[:XXII],
+      },
+
+      # read only variables
+      ARO_ENV_O: {
+        type: :string,
+        access: :read,
+        description: I18n.t(
+          "cli.config.aro_env_O_description"
+        ),
+        value: Aro::Mancy::O
+      },
+      ARO_ENV_S: {
+        type: :string,
+        access: :read,
+        description: I18n.t(
+          "cli.config.aro_env_S_description"
+        ),
+        value: Aro::Mancy::S
+      },
+      ARO_ENV_N: {
+        type: :string,
+        access: :read,
+        description: I18n.t(
+          "cli.config.aro_env_N_description"
+        ),
+        value: Aro::Mancy::N
+      },
+      ARO_ENV_PS1: {
+        type: :string,
+        access: :read,
+        description: I18n.t(
+          "cli.config.aro_env_PS1_description"
+        ),
+        value: Aro::Mancy::PS1
+      },
+      ARO_ENV_NAME_FILE: {
+        type: :string,
+        access: :read,
+        description: I18n.t(
+          "cli.config.aro_env_NAME_FILE_description"
+        ),
+        value: Aro::Mancy::NAME_FILE
+      },
+      ARO_ENV_I2097I: {
+        type: :string,
+        access: :read,
+        description: I18n.t(
+          "cli.config.aro_env_I2097I_description"
+        ),
+        value: Aro::Mancy::I2097I
+      },
+      ARO_ENV_YES: {
+        type: :string,
+        access: :read,
+        description: I18n.t(
+          "cli.config.aro_env_YES_description"
+        ),
+        value: Aro::Mancy::YES
       },
 
       # ...
     }
 
+    def print_cr f_object_id
+      file = ObjectSpace._id2ref f_object_id
+      Aro::Mancy::OS.times do
+        file.write("#\n")
+      end
+    end
+
+    def print_config_header f_object_id
+      file = ObjectSpace._id2ref f_object_id
+      file.write("# #{Aro::Mancy::PS1} configuration file.\n")
+      file.write("# this file was auto generated by the aro cli.\n")
+    end
+
+    def print_def_types f_object_id
+      file = ObjectSpace._id2ref f_object_id
+      file.write("# CLI::Config::DEF_TYPES\n")
+      file.write("# describes the possible types of variables.\n")
+      CLI::Config::DEF_TYPES.each{|k, v|
+        file.write("# #{k}: #{v[:description]}\n")
+      }
+      print_cr file.object_id
+      file.write("# CLI::Config::DEF\n")
+      file.write("# define & expose an aro bash api via ENV variables.\n")
+      file.write("# modifying them in this file will change aro's behavior.\n")
+    end
+
+    def print_var f_object_id, k, v
+      file = ObjectSpace._id2ref f_object_id
+
+      if CLI::Config::DEF[k][:access] == CLI::Config::DEF_ACCESS[:READ]
+        var_name = k
+      else
+        var_name = CLI::Config.cvar_k(k)
+      end
+      Aro::D.say("access for #{k} is #{CLI::Config::DEF[k][:access]}")
+      Aro::D.say("using var_name: #{var_name}")
+      file.write("# [#{var_name}]\n")
+      file.write("#   => #{I18n.t("cli.config.def_type")}: #{v[:type]}\n")
+      case v[:type]
+      when CLI::Config::DEF_TYPES[:INT][:name]
+        file.write("#   => #{I18n.t("cli.config.minimum")}: #{v[:min]}\n")
+        file.write("#   => #{I18n.t("cli.config.maximum")}: #{v[:max]}\n")
+      when CLI::Config::DEF_TYPES[:STRING][:name]
+        file.write("#   => #{I18n.t("cli.config.type_string_description")}\n")
+        file.write("#   => use \"double quotes\" if there are any spaces.\n")
+      when CLI::Config::DEF_TYPES[:VALUES][:name]
+        file.write("#   => #{I18n.t("cli.config.possible_values")}:\n")
+        v[:possible_values].each{|name, description|
+          file.write("#   => #{name} - #{description}\n")
+        }
+      end
+      file.write("export #{var_name}=#{v[:value]}\n")
+      print_cr file.object_id
+    end
+
     def generate_config
       # todo: localize generated config text
-      Aro::P.say(I18n.t("cli.config.generating_default_config", name: CLI::Config::CONFIG_FILE_PATH))
-      File.open(CLI::Config::CONFIG_FILE_PATH, "w+") do |file|
-        file.write("# #{Aro::Mancy::PS1} configuration file.\n")
-        file.write("# this file was auto generated by the aro cli.\n")
-        Aro::Mancy::OS.times do
-          file.write("#\n")
-        end
-        file.write("# CLI::Config::DEF_TYPES\n")
-        file.write("# describes the possible types of variables.\n")
-        CLI::Config::DEF_TYPES.each{|k, v|
-          file.write("# #{k}: #{v[:description]}\n")
-        }
-        Aro::Mancy::OS.times do
-          file.write("#\n")
-        end
-        file.write("# CLI::Config::DEF\n")
-        file.write("# define & expose an aro bash api via ENV variables.\n")
-        file.write("# modifying them in this file will change aro's behavior.\n")
-        Aro::Mancy::OS.times do
-          file.write("#\n")
-        end
+      Aro::D.say(I18n.t("cli.config.generating_default_config", name: CLI::Config.config_filepath))
+      File.open(CLI::Config.config_filepath, "w+") do |file|
+        print_config_header file.object_id
+        print_cr file.object_id
+        print_def_types file.object_id
+        print_cr file.object_id
         CLI::Config::DEF.each{|k, v|
-          var_name = CLI::Config.var_key_with_suffix(k)
-          file.write("# [#{var_name}]\n")
-          file.write("#   => #{I18n.t("cli.config.def_type")}: #{v[:type]}\n")
-          case v[:type]
-          when CLI::Config::DEF_TYPES[:INT][:name]
-            file.write("#   => #{I18n.t("cli.config.minimum")}: #{v[:min]}\n")
-            file.write("#   => #{I18n.t("cli.config.maximum")}: #{v[:max]}\n")
-          when CLI::Config::DEF_TYPES[:VALUES][:name]
-            file.write("#   => #{I18n.t("cli.config.possible_values")}:\n")
-            v[:possible_values].each{|name, description|
-              file.write("#   => #{name} - #{description}\n")
-            }
-          end
-          file.write("export #{var_name}=#{v[:value]}\n\n")
+          print_var file.object_id, k, v
         }
-        file.write("\n")
+        print_cr file.object_id
       end
     end
 
     def initialize
-      return unless Aro::Mancy.is_aro_space?
-      unless File.exist?(CLI::Config::CONFIG_FILE_PATH)
+      return unless Aro::Mancy.is_aro_space? && Aro::Mancy.is_initialized?
+      unless File.exist?(CLI::Config.config_filepath)
         generate_config
       end
 
       source_config
     end
 
-    def self.def_valid?(deff, key)
-      deff == CLI::Config::DEF[key]
+    def self.display_config
+      Aro::D.say("getting display config...")
+      columns = CLI::Config.cvar(:DISPLAY_COLUMNS)
+      Aro::D.say("DISPLAY_COLUMNS: #{columns}")
+
+      # default
+      width = CLI::Config::DEF[:DISPLAY_COLUMNS][:value]
+      if columns.respond_to?(:to_i)
+        # user setting
+        columns = columns.to_i
+        width = columns.to_i.pow(Aro::Mancy::OS)
+      end
+      Aro::D.say("WIDTH: #{width}")
+
+      {
+        COLUMNS: columns,
+        WIDTH: width,
+        DIVIDER: :"-".to_s
+      }
+    end
+
+    def self.config_filepath
+      File.join(Aro::Db.base_aro_dir, CLI::Config::CONFIG_FILE.to_s)
+    end
+
+    def self.def_valid?(key, deff)
+      def_valid = deff == CLI::Config::DEF[key]
+      unless def_valid
+        Aro::D.say("invalid def! #{key} => #{deff}")
+      end
+
+      def_valid
     end
 
     def self.int_valid?(unvalid)
-      return !unvalid.nil? && (
-        unvalid.kind_of?(Integer) ||
-        ("0" != unvalid && unvalid.respond_to?(:to_i))
-      )
+      !unvalid&.to_i.nil?
     end
 
-    def self.var_value_with_suffix(suffix)
-      ENV[CLI::Config.var_key_with_suffix(suffix)]
+    def self.evar(suffix)
+      ENV[CLI::Config.evar_k(suffix)]
     end
 
-    def self.var_key_with_suffix(suffix)
-      "#{CLI::Config::VAR_PREFIX}#{suffix}"
+    def self.evar_k(suffix)
+      "#{CLI::Config::ARO_ENV_PREFIX}#{suffix}"
+    end
+
+    def self.cvar(suffix)
+      ENV[CLI::Config.cvar_k(suffix)]
+    end
+
+    def self.cvar_k(suffix)
+      "#{CLI::Config::ARO_CONFIG_PREFIX}#{suffix}"
     end
 
     def source_config
-      Aro::P.say(I18n.t("cli.config.sourcing_config", name: CLI::Config::CONFIG_FILE_PATH))
-      File.read(CLI::Config::CONFIG_FILE_PATH).split("\n").select{|line|
-        line.match?("export #{CLI::Config::VAR_PREFIX}")
+      Aro::D.say(I18n.t("cli.config.sourcing_config", name: CLI::Config.config_filepath))
+      File.read(CLI::Config.config_filepath).split("\n").select{|line|
+        line.match?(/export #{:ARO_}/)
       }.map{|line| 
         line.gsub("export ", "").split("=")
       }.each{|kv|
-        ENV[kv[0]] = kv[1]
+        Aro::D.say("variable to set: #{kv}")
+        ENV[kv[0]] = kv[1] # source
+        Aro::D.say("value actually set: #{ENV[kv[0]]}")
       }
       
       # todo: implement
-      # invalid_defs = validate_config
+      invalid_defs = validate_config
       CLI::Config::DEF.keys.each{|dfk|
-        # next if invalid_defs.include?(dfk)
-        Aro::P.say("$#{CLI::Config.var_key_with_suffix(dfk)}=#{CLI::Config.var_value_with_suffix(dfk)}")
+        Aro::D.say("$#{CLI::Config.cvar_k(dfk)}=#{CLI::Config.cvar(dfk)}")
       }
+      Aro::D.say("invalid_defs: #{invalid_defs}")
 
       # todo: set all invalid_refs to default in ENV
-    end
+      # invalid_defs.each{|id| 
 
-    def validate_config
-      invalid_defs = []
-      CLI::Config::DEF.each{|k, v|
-        var_value = CLI::Config.var_value_with_suffix(k)
-        is_valid = CLI::Config::DEF_TYPES[v[:type].to_s.upcase.to_sym][:validator]
-          .call(var_value, v, k)
-        invalid_defs << k unless is_valid
-      }
-      invalid_defs
+      # }
     end
 
   end
---
+++ bin/cli/nterface.rb
@@ -0,0 +1,25 @@
+=begin
+  
+  nterface.rb
+
+  cli nterface.
+
+  by i2097i
+
+=end
+
+module CLI
+  class Nterface
+    def self.exit_error_missing_args!
+      Aro::P.say(I18n.t("cli.errors.header", cmd: Aro::Mancy::I2097I))
+      Aro::P.say(I18n.t("cli.errors.missing_args", cmd: Aro::Mancy::I2097I))
+      exit(CLI::EXIT_CODES[:INVALID_ARG])
+    end
+
+    def self.exit_error_invalid_usage!
+      Aro::P.say(I18n.t("cli.errors.header", cmd: Aro::Mancy::I2097I))
+      Aro::P.say(I18n.t("cli.errors.invalid_usage", cmd: Aro::Mancy::I2097I))
+      exit(CLI::EXIT_CODES[:INVALID_ARG])
+    end
+  end
+end
