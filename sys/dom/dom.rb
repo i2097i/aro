4ff2475c362c177d88111e42cd553325640e28ca
i2097i <i2097i@hotmail.com>
2025-12-07 03:53:12 -0500

    created aos
    rename(ivar): ARO_CONFIG_DISPLAY_COLUMN -> ARO_CONFIG_WIDTH
    add(ivar): ARO_CONFIG_HEIGHT

--- sys/dom/dom.rb
+++ sys/dom/dom.rb
@@ -103,7 +103,6 @@ module Aro
       search_path.any?{|step|
         search_pwd = File.join(search_pwd, step)
         ls = Dir.glob("#{search_pwd}/#{ETHER_FILE}", File::FNM_DOTMATCH)
-
         path = ls.first if ls.any?
         !path.nil?
       }
